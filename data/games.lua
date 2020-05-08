ac0b408cd9e7eb0c1fdfea3e702b59d5de1737b5
ceg <37312389+Vzurxy@users.noreply.github.com>
2020-05-08 15:37:01 -0400

    Update games.lua

--- data/games.lua
+++ data/games.lua
@@ -1,6 +1,12 @@
+local BASE_SCRIPTS = "https://raw.githubusercontent.com/Vzurxy/zerohub/master/scripts/"
+
+local function GetScript(Name)
+  return BASE_SCRIPTS .. Name .. ".lua"
+end
+
 return {
   ["000000000"] = {
     Timestamp = "2020-00-00T0:00:00.00Z",
-    Script = "https://raw.githubusercontent.com/Vzurxy/zerohub/master/scripts/example.lua"
+    Script = GetScript("example")
   }
 }
