6a46aed189b64eadccbaf7dd6151356253b16550
ceg <37312389+Vzurxy@users.noreply.github.com>
2020-05-18 14:26:56 -0400

    Update games.lua

--- data/games.lua
+++ data/games.lua
@@ -1,10 +1,8 @@
 local BASE_SCRIPTS = "https://raw.githubusercontent.com/Vzurxy/zerohub/master/scripts/"
 
 local function GetScript(Name)
-  return BASE_SCRIPTS .. Name .. ".lua"
+    return BASE_SCRIPTS .. Name .. ".lua"
 end
-
-warn'mien craft'
 
 return {
   ["000000000"] = {
@@ -14,8 +12,8 @@ return {
   ["1962086868"] = {
         Timestamp = "2020-05-08T14:02:32.9696902Z",
         Script = GetScript("TowerOfHell")
-  }
+  },
   ["Universal"] = {
-    Script = GetScript("Universal")
-   }
+        Script = GetScript("Universal")
+  }
 }
